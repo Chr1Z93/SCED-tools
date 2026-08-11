94aa0b05ae566321ee07a1d18958f2908937c4e3
Chr1Z93 <puls.christian@gmail.com>
2026-08-11 11:15:57 +0200

    updated shoggoth script

--- scripts/shoggoth-tools/update-tts-ids.py
+++ scripts/shoggoth-tools/update-tts-ids.py
@@ -1,26 +1,70 @@
 import json
+import os
 from pathlib import Path
 
-english_path = Path()
-german_path = Path()
-output_path = Path()
-name_map_path = Path()
+english_folder = Path(
+    r"C:\git\SCED-downloads\decomposed\campaign\Alice in Wonderland\AliceinWonderland.39916d"
+)
+german_folder = Path(
+    r"C:\git\SCED-downloads\decomposed\language-pack\German - Fan Campaigns\German-FanCampaigns.GermanFC\AliceinWonderland.209aaa"
+)
+name_map_path = Path("alice_mapping.json")
+
+
+def get_metadata_obj(file_path, data):
+    base_name = os.path.splitext(file_path)[0]
+    gmnotes_file = base_name + ".gmnotes"
+
+    if os.path.exists(gmnotes_file):
+        with open(gmnotes_file, "r", encoding="utf-8") as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                return {}
+
+    try:
+        return json.loads(data.get("GMNotes", ""))
+    except (json.JSONDecodeError, TypeError):
+        return {}
 
 
 def update_tts_ids():
-    # load english saved object
+    # load existing map of english name -> german name
+    with open(name_map_path, "r", encoding="utf-8") as f:
+        english_to_german_name = json.load(f)
 
-    # create a map of english name -> ID
+    # create a map of german name -> ID
+    english_name_to_id = {}
+    for root, dirs, files in os.walk(english_folder):
+        for filename in files:
+            if not filename.endswith(".json"):
+                continue
 
-    # load german saved object
+            file_path = os.path.join(root, filename)
 
-    # loop through cards, updating every ID based on the two maps
+            with open(file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+
+            metadata = get_metadata_obj(file_path, data)
+            name = data["Nickname"]
+
+            if name not in english_to_german_name:
+                print(f"No translation found for {name}")
+                continue
+
+            german_name = english_to_german_name[name]
+            id = metadata.get("id", metadata.get("TtsZoopGuid"))
+
+            if not id:
+                print(f"No ID found for {name}")
+                continue
+
+            english_name_to_id[german_name] = id
+
+    # loop through german cards, updating every ID based on the two maps
     # 1: german name -> english name | 2: english name -> ID
     # if no match found, change ID to "NOT-FOUND"
     # if multiple cards with the same name BUT different original ID are found, change ID to "CONFLICT" (for all cards with that name)
-
-    # save updated file as output
-    return
 
 
 if __name__ == "__main__":
